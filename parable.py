3da6276d41ea7527fb34cdd76506333da94ac958
Mostafa Razavi <mostafa@sepent.com>
2015-01-29 15:01:57 +0330

    String and integer literals added.
    String literals (represented as quoted strings) and integer literals
    added. They are read as Python strings and integers
    respectively. Symbols are now returned as instances of the newly added
    Symbol class.

--- parable.py
+++ parable.py
@@ -1,7 +1,17 @@
 from cStringIO import StringIO
 
+class Symbol(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __eq__(self, other):
+        return type(other) == Symbol and self.name == other.name
+
+    def __hash__(self):
+        return hash(self.name)
+
 def eval_if(sexp, env):
-    assert sexp[0] == 'if'
+    assert sexp[0].name == 'if'
     assert len(sexp) == 4
     cond = eval(sexp[1], env)
     if cond == []:
@@ -10,12 +20,12 @@ def eval_if(sexp, env):
         return eval(sexp[2], env)
 
 def eval_quote(sexp, env):
-    assert sexp[0] == 'quote'
+    assert sexp[0].name == 'quote'
     assert len(sexp) == 2
     return sexp[1]
 
 def eval_atom(sexp, env):
-    assert sexp[0] == 'atom'
+    assert sexp[0].name == 'atom'
     val = eval(sexp[1], env)
     if type(val) == list:
         return []
@@ -23,21 +33,21 @@ def eval_atom(sexp, env):
         return 't'
 
 def eval_car(sexp, env):
-    assert sexp[0] == 'car'
+    assert sexp[0].name == 'car'
     assert len(sexp) == 2
     ret = eval(sexp[1], env)
     assert type(ret) == list
     return ret[0]
 
 def eval_cdr(sexp, env):
-    assert sexp[0] == 'cdr'
+    assert sexp[0].name == 'cdr'
     assert len(sexp) == 2
     ret = eval(sexp[1], env)
     assert type(ret) == list
     return ret[1:]
 
 def eval_cons(sexp, env):
-    assert sexp[0] == 'cons'
+    assert sexp[0].name == 'cons'
     assert len(sexp) == 3
     car = eval(sexp[1], env)
     cdr = eval(sexp[2], env)
@@ -45,7 +55,7 @@ def eval_cons(sexp, env):
     return [car] + cdr
 
 def eval_eq(sexp, env):
-    assert sexp[0] == 'eq'
+    assert sexp[0].name == 'eq'
     assert len(sexp) == 3
     first = eval(sexp[1], env)
     second = eval(sexp[2], env)
@@ -57,9 +67,13 @@ def eval_eq(sexp, env):
 def eval(exp, env):
     if type(exp) == list:
         return eval_sexp(exp, env)
+    elif type(exp) == int:
+        return exp
+    elif type(exp) == str:
+        return exp
 
     if exp not in env:
-        print 'Undefined variable:', exp
+        print 'Undefined variable:', exp.name
         exit(1)
 
     return env[exp]
@@ -70,19 +84,19 @@ def eval_sexp(sexp, env):
 
     car = sexp[0]
 
-    if car in ('fn', 'mac'):
+    if car in (Symbol('fn'), Symbol('mac')):
         assert len(sexp) == 3
         return sexp
 
-    map = {'if': eval_if,
-           'quote': eval_quote,
-           'cons': eval_cons,
-           'atom': eval_atom,
-           'car': eval_car,
-           'cdr': eval_cdr,
-           'eq': eval_eq}
+    map = {Symbol('if'): eval_if,
+           Symbol('quote'): eval_quote,
+           Symbol('cons'): eval_cons,
+           Symbol('atom'): eval_atom,
+           Symbol('car'): eval_car,
+           Symbol('cdr'): eval_cdr,
+           Symbol('eq'): eval_eq}
 
-    if type(car) == str and car in map:
+    if type(car) == Symbol and car in map:
         return map[car](sexp, env)
 
     # it must be a function or macro call then.
@@ -100,12 +114,12 @@ def eval_sexp(sexp, env):
         print 'Expected {} argument(s) but got {}.'.format(len(params), len(args))
         exit(2)
 
-    if car[0] == 'fn':
+    if car[0] == Symbol('fn'):
         # evaluate arguments.
         args = [eval(i, env) for i in args]
 
         return eval(body, dict(env, **dict(zip(params, args))))
-    elif car[0] == 'mac':
+    elif car[0] == Symbol('mac'):
         expanded = eval(body, dict(env, **dict(zip(params, args))))
         return eval(expanded, env)
     else:
@@ -118,11 +132,24 @@ def read_item(f):
     while b and b not in '() \n':
         item += b
         b = f.read(1)
-    if not b:
-        print 'Unexpected end of file.'
-        exit(1)
-    f.seek(-1, 1)
-    return item
+
+    if b:
+        f.seek(-1, 1)
+
+    try:
+        # is this an integer?
+        integer = int(item)
+
+        # yes, it is.
+        return integer
+    except:
+        pass # not an integer
+
+    # is it a string literal?
+    if len(item) >= 2 and item[0] == '"' and item[-1] == '"':
+        return item[1:-1]
+
+    return Symbol(item)
 
 def read_list(f):
     b = f.read(1)
--- test.py
+++ test.py
@@ -1,25 +1,52 @@
 import parable
+from parable import Symbol
 
 import unittest
+
+class SymbolTest(unittest.TestCase):
+    def test_equal(self):
+        self.assertEqual(Symbol('foo'), Symbol('foo'))
+
+    def test_not_equal(self):
+        self.assertNotEqual(Symbol('foo'), Symbol('bar'))
+
+    def test_hash(self):
+        d = {}
+        d[Symbol('foo')] = 1000
+        d[Symbol('foo')] = 2000
+        d[Symbol('bar')] = 3000
+
+        self.assertEqual(d[Symbol('foo')], 2000)
+        self.assertEqual(d[Symbol('bar')], 3000)
 
 class ParableCoreTest(unittest.TestCase):
     def eval_str(self, s):
         exp = parable.parse_str(s)
         return parable.eval(exp, {})
 
+    def test_integer(self):
+        exp = '1080'
+        result = self.eval_str(exp)
+        self.assertEqual(result, 1080)
+
+    def test_string(self):
+        exp = '"foobar"'
+        result = self.eval_str(exp)
+        self.assertEqual(result, 'foobar')
+
     def test_if(self):
         exp = '(if (quote t) (quote a) (quote b))'
         result = self.eval_str(exp)
-        self.assertEqual(result, 'a')
+        self.assertEqual(result, Symbol('a'))
 
         exp = '(if (quote ()) (quote a) (quote b))'
         result = self.eval_str(exp)
-        self.assertEqual(result, 'b')
+        self.assertEqual(result, Symbol('b'))
 
     def test_quote(self):
         exp = '(quote x)'
         result = self.eval_str(exp)
-        self.assertEqual(result, 'x')
+        self.assertEqual(result, Symbol('x'))
 
     def test_atom(self):
         exp = '(atom (quote x))'
@@ -33,17 +60,17 @@ class ParableCoreTest(unittest.TestCase):
     def test_car(self):
         exp = '(car (quote (x y z)))'
         result = self.eval_str(exp)
-        self.assertEqual(result, 'x')
+        self.assertEqual(result, Symbol('x'))
 
     def test_cdr(self):
         exp = '(cdr (quote (x y z)))'
         result = self.eval_str(exp)
-        self.assertEqual(result, ['y', 'z'])
+        self.assertEqual(result, [Symbol('y'), Symbol('z')])
 
     def test_cons(self):
         exp = '(cons (quote x) (quote (y z)))'
         result = self.eval_str(exp)
-        self.assertEqual(result, ['x', 'y', 'z'])
+        self.assertEqual(result, [Symbol(i) for i in ['x', 'y', 'z']])
 
     def test_eq(self):
         exp = '(eq (quote x) (quote x))'
@@ -61,12 +88,12 @@ class ParableCoreTest(unittest.TestCase):
     def test_function_call(self):
         exp = '((fn (a b c) b) (quote a) (quote b) (quote c))'
         result = self.eval_str(exp)
-        self.assertEqual(result, 'b')
+        self.assertEqual(result, Symbol('b'))
 
     def test_macro_call(self):
         exp = '((mac (a b c) b) (a b) (if (quote t) (quote a) (quote b)) (p q))'
         result = self.eval_str(exp)
-        self.assertEqual(result, 'a')
+        self.assertEqual(result, Symbol('a'))
 
 if __name__ == '__main__':
     unittest.main()
