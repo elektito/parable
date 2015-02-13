ccd584ca86c7d0a756f6b7f2dc5f196a748a7594
Mostafa Razavi <mostafa@sepent.com>
2015-02-13 13:24:26 +0330

    Test runner now shows failed assertions. Closes #32.

--- load.py
+++ load.py
@@ -12,6 +12,61 @@ class LoadError(RuntimeError):
         super(LoadError, self).__init__(msg)
         self.form = form
 
+def display_form(form, context):
+    with open(form.filename) as f:
+        all_lines = f.readlines()
+
+    lines = zip(xrange(form.start_row, form.end_row + 1),
+                all_lines[form.start_row:form.end_row + 1])
+
+    if not context:
+        pass
+    elif form.start_row == 1:
+        lines = [(0, all_lines[0])] + lines
+    elif form.start_row > 1:
+        lines = [(form.start_row - 2, all_lines[form.start_row - 2]),
+                 (form.start_row - 1, all_lines[form.start_row - 1])] + lines
+
+    if not context:
+        pass
+    elif form.end_row == len(all_lines) - 2:
+        lines.append((len(all_lines - 2), all_lines[-2]))
+    elif form.end_row < len(all_lines) - 2:
+        lines.extend([(form.end_row + 1, all_lines[form.end_row + 1]),
+                      (form.end_row + 2, all_lines[form.end_row + 2])])
+
+    d = dict(
+        LINE_NO_COLOR = '\033[95m',
+        CODE_COLOR = '\033[92m',
+        HIGHLIGHT_COLOR = '\033[93m',
+        END_COLOR = '\033[0m')
+
+    for lineno, line in lines:
+        line = line[:-1] if line.endswith('\n') else line
+        if lineno < form.start_row or lineno > form.end_row:
+            first_part = line
+            second_part = third_part = ''
+        elif form.start_row == lineno and form.end_row != lineno:
+            first_part=line[:form.start_col]
+            second_part=line[form.start_col:]
+            third_part=''
+        elif form.end_row == lineno and form.start_row != lineno:
+            first_part=''
+            second_part=line[:form.end_col+1]
+            third_part=line[form.end_col+1:]
+        else:
+            first_part=line[:form.start_col]
+            second_part=line[form.start_col:form.end_col+1]
+            third_part=line[form.end_col+1:]
+
+        d2 = dict(lineno=lineno,
+                  first_part=first_part,
+                  second_part=second_part,
+                  third_part=third_part)
+        print '{LINE_NO_COLOR}{lineno}: ' \
+            '{CODE_COLOR}{first_part}{HIGHLIGHT_COLOR}' \
+            '{second_part}{CODE_COLOR}{third_part}{END_COLOR}'.format(**dict(d, **d2))
+
 def print_error(e):
     if not hasattr(e, 'form') or \
        not all(hasattr(e.form, a) for a in ['start_row',
@@ -25,55 +80,7 @@ def print_error(e):
 
     print 'Error:', e
 
-    with open(e.form.filename) as f:
-        all_lines = f.readlines()
-
-    lines = zip(xrange(e.form.start_row, e.form.end_row + 1),
-                all_lines[e.form.start_row:e.form.end_row + 1])
-
-    if e.form.start_row == 1:
-        lines = [(0, all_lines[0])] + lines
-    elif e.form.start_row > 1:
-        lines = [(e.form.start_row - 2, all_lines[e.form.start_row - 2]),
-                 (e.form.start_row - 1, all_lines[e.form.start_row - 1])] + lines
-
-    if e.form.end_row == len(all_lines) - 2:
-        lines.append((len(all_lines - 2), all_lines[-2]))
-    elif e.form.end_row < len(all_lines) - 2:
-        lines.extend([(e.form.end_row + 1, all_lines[e.form.end_row + 1]),
-                      (e.form.end_row + 2, all_lines[e.form.end_row + 2])])
-
-    d = dict(
-        LINE_NO_COLOR = '\033[95m',
-        CODE_COLOR = '\033[92m',
-        HIGHLIGHT_COLOR = '\033[93m',
-        END_COLOR = '\033[0m')
-
-    for lineno, line in lines:
-        line = line[:-1] if line.endswith('\n') else line
-        if lineno < e.form.start_row or lineno > e.form.end_row:
-            first_part = line
-            second_part = third_part = ''
-        elif e.form.start_row == lineno and e.form.end_row != lineno:
-            first_part=line[:e.form.start_col]
-            second_part=line[e.form.start_col:]
-            third_part=''
-        elif e.form.end_row == lineno and e.form.start_row != lineno:
-            first_part=''
-            second_part=line[:e.form.end_col+1]
-            third_part=line[e.form.end_col+1:]
-        else:
-            first_part=line[:e.form.start_col]
-            second_part=line[e.form.start_col:e.form.end_col+1]
-            third_part=line[e.form.end_col+1:]
-
-        d2 = dict(lineno=lineno,
-                  first_part=first_part,
-                  second_part=second_part,
-                  third_part=third_part)
-        print '{LINE_NO_COLOR}{lineno}: ' \
-            '{CODE_COLOR}{first_part}{HIGHLIGHT_COLOR}' \
-            '{second_part}{CODE_COLOR}{third_part}{END_COLOR}'.format(**dict(d, **d2))
+    display_form(e.form, True)
 
 def run_tests(f, filename, env):
     passed = 0
@@ -91,7 +98,8 @@ def run_tests(f, filename, env):
             result = eval_form(form, env)
             if result == []:
                 failed += 1
-                print 'Test failed.'
+                print 'Test failed:'
+                display_form(form, False)
             elif result == Symbol('t'):
                 passed += 1
                 print 'Test passed.'
